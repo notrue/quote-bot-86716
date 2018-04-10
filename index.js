193e6ea5241b38c9f6cf0214083db506a4273b79
Bukchin <yoni.bukchin@hpe.com>
2018-04-10 22:03:29 +0300

    use channel to delete msgs

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ bot.on('message', (message) => {
             || upMsg === '/clear'.toUpperCase() 
             || upMsg === '/delete'.toUpperCase()) {
                 message.reply('Deleting... please wait');
-                console.log('webhook: ' + !message.webhookID);
+                console.log('webhook: ' + message.webhookID + ', ' + !message.webhookID);
                 if (!message.webhookID && message.member.hasPermission("MANAGE_MESSAGES")) {
                     message.channel.fetchMessages()
                        .then(function(list){
